9afe6ef7fa164d3534231e81540ea2276da1c56b
tahmid choudhury <tahmidachoudhury@outlook.com>
2024-03-04 16:26:54 +0000

    commit to switch branches

--- tests/test_space_repository.py
+++ tests/test_space_repository.py
@@ -1,2 +1,18 @@
 from lib.space_repository import *
+from lib.space import *
 
+""""
+.all()
+attribute should retrieve all spaces on the database with their data presented neatly.
+
+space(1, "appt1", ...)
+
+"""
+def test_get_all_spaces(db_connection):
+    db_connection.seed("seeds/spaces_table.sql")
+    repository = SpaceRepository(db_connection)
+    assert repository.all() == [
+        Space(1, 'appt 1', '2024-05-12', 'london', 50.00, 'jbvsdjh', 1),
+        Space(1, 'appt 1', '2024-05-12', 'london', 50.00, 'jbvsdjh', 1)
+        
+    ]
