4baba25899914da02178016d133f6e0a5b056e80
Danny Castilla <dancastiv@gmail.com>
2024-03-04 16:18:07 +0000

    made spaces table

--- seeds/spaces_table.sql
+++ seeds/spaces_table.sql
@@ -2,7 +2,12 @@
 -- And add any data that is needed for the tests to run.
 -- This is so that our tests, and application, are always operating from a fresh
 -- database state, and that tests don't interfere with each other.
+DROP TABLE IF EXISTS spaces;
+DROP SEQUENCE IF EXISTS spaces_id_seq;
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS users_id_seq;
 
+CREATE SEQUENCE IF NOT EXISTS spaces_id_seq;
 CREATE TABLE spaces (
   id SERIAL PRIMARY KEY,
   name text,
@@ -14,5 +19,7 @@ CREATE TABLE spaces (
 );
 
 -- Finally, we add any records that are needed for the tests to run
-INSERT INTO spaces (id, name, date_booked, location, user_id) VALUES (1, 'appt 1', '2024-05-12', 'london', 50.00, 'jbvsdjh', 1);
-INSERT INTO spaces (id, name, date_booked, location, user_id) VALUES (2, 'appt 2', '2024-07-12', 'paris', 87.00, 'djfhfh', 2);
+INSERT INTO spaces (name, booking_date, location, price, description, user_id) VALUES ('Wizarding Cupboard', '2024-05-12', 'London', 50.00, 'A cosy room under the stairs. Comes with complementary spiders.', 1);
+INSERT INTO spaces (name, booking_date, location, price, description, user_id) VALUES ('Amore Penthouse', '2024-07-12', 'Paris', 87.00, 'Within view of the Eiffel Tower, this penthouse is your parisian dream.', 2);
+INSERT INTO spaces (name, booking_date, location, price, description, user_id) VALUES ('Paella Place', '2024-07-14', 'Madrid', 31.59, 'Eat paella and sleep.', 3);
+INSERT INTO spaces (name, booking_date, location, price, description, user_id) VALUES ('Mi Casa', '2024-07-12', 'Madrid', 45.50, 'Es tu Casa.', 3);
